37553b69ab710df9f27daec887776da0e77f88f9
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-05-24 20:44:25 +0200

    created wpf application and connected it with db

---
+++ HCI_main_project/HCI_main_project/Models/TestTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HCI_main_project.Models;
+
+public partial class TestTable
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+}
