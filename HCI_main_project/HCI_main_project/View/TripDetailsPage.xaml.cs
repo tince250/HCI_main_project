045e0789f47bcc4fb693a9e37c3eed4f97cb3f39
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-06-06 18:21:23 +0200

    added pins for tour parts in trip details and fixed button design bugs

--- HCI_main_project/HCI_main_project/View/TripDetailsPage.xaml.cs
+++ HCI_main_project/HCI_main_project/View/TripDetailsPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using HCI_main_project.ViewModel;
+﻿using HCI_main_project.Models;
+using HCI_main_project.ViewModel;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maps.MapControl.WPF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,11 +25,53 @@ namespace HCI_main_project.View
     /// </summary>
     public partial class TripDetails : Page
     {
+        private TripDetailsViewModel viewModel;
         public TripDetails()
         {
             InitializeComponent();
-            DataContext = App.serviceProvider.GetRequiredService<TripDetailsViewModel>();
+            this.viewModel = App.serviceProvider.GetRequiredService<TripDetailsViewModel>();
+            DataContext = viewModel;
+            setPushPins();
+        }
 
+        private void setPushPins()
+        {
+            setRestaurantPushPins();
+            setAccommodationsPushPins();
+            setAttractionsPushPins();
+        }
+
+        private void setRestaurantPushPins()
+        {
+            foreach (Restaurant restaurant in viewModel.Restaurants)
+            {
+                Pushpin Pin = new Microsoft.Maps.MapControl.WPF.Pushpin();
+                Pin.Location = new Location(restaurant.Latitude, restaurant.Longitude);
+                Pin.Background = new SolidColorBrush(Color.FromRgb(24, 179, 21));
+                this.map.Children.Add(Pin);
+            }
+        }
+
+        private void setAttractionsPushPins()
+        {
+            foreach (Attraction attraction in viewModel.Attractions)
+            {
+                Pushpin Pin = new Microsoft.Maps.MapControl.WPF.Pushpin();
+                Pin.Location = new Location(attraction.Latitude, attraction.Longitude);
+                Pin.Background = new SolidColorBrush(Color.FromRgb(39, 117, 185));
+                this.map.Children.Add(Pin);
+            }
+        }
+
+        private void setAccommodationsPushPins()
+        {
+            foreach (Accommodation accommodation in viewModel.Accommodations)
+            {
+                Pushpin Pin = new Microsoft.Maps.MapControl.WPF.Pushpin();
+                Pin.Location = new Location(accommodation.Latitude, accommodation.Longitude);
+                Pin.Background = new SolidColorBrush(Color.FromRgb(232, 170, 42));
+                this.map.Children.Add(Pin);
+            }
         }
     }
 }
