411d38aaf0e6829efa589f98ad6d96f5434b0dc1
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-06-11 21:37:23 +0200

    created user card component and content entity for its bindings, as well as converter from content to card

---
+++ HCI_main_project/HCI_main_project/Model/Entities/UserCardContent.cs
@@ -0,0 +1,26 @@
+﻿using HCI_main_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCI_main_project.Model.Entities
+{
+    public class UserCardContent
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+
+        public UserCardContent(User user, BookingStatus status)
+        {
+            Name = user.FirstName + " " + user.LastName;
+            Email = user.Email;
+            if (status == BookingStatus.BOOKING)
+                this.Status = "BOOKED";
+            else
+                this.Status = "RESERVED";
+        }
+    }
+}
