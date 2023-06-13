f45482bf055c0752ffde06d5044b73c14f313fca
tince250 <t.s.mihajlovic25@gmail.com>
2023-06-13 20:40:45 +0200

    added data.sql file

---
+++ data.sql
@@ -0,0 +1,14 @@
+INSERT into Addresses (City, PostalCode, Street, StreetNumber) values ("Belgrade", 21000, "Kneginje Ljubice", 18);
+INSERT into Addresses (City, PostalCode, Street, StreetNumber) values ("Belgrade", 21000, "Jevrejska", 22);
+INSERT into Addresses (City, PostalCode, Street, StreetNumber) values ("Belgrade", 21000, "Vladimira Popovica", 6);
+INSERT into Addresses (City, PostalCode, Street, StreetNumber) values ("Belgrade", 21000, "Dobropoljska", 65);
+INSERT into Addresses (City, PostalCode, Street, StreetNumber) values ("Belgrade", 21000, "Carigradska", 3);
+
+insert into Users (FirstName, LastName, Email, Password, "Role") values ("Agent", "Agentovic", "agent@gmail.com", "12345678", 1);
+insert into Users (FirstName, LastName, Email, Password, "Role") values ("User", "Userovic", "user@gmail.com", "12345678", 0);
+
+insert into Restaurants (Name, Image, AddressId, Latitude, Longitude) values ("Sushi bar", "sushi.jpg", 3, 44.821500, 20.463789);
+insert into Attractions (Name, Image, AddressId, Latitude, Longitude) values ("Fortress", "fortress.jpg", 4, 44.827242, 20.457272);
+insert into Attractions (Name, Image, AddressId, Latitude, Longitude) values ("Belgrade zoo", "zoo.jpg", 5, 44.810401, 20.437032);
+insert into Accommodations (Name, Image, AddressId, Latitude, Longitude, Type) values ("Hotel Moscow", "moscow.jpg", 6, 44.792210, 20.462648, 0);
+insert into Accommodations (Name, Image, AddressId, Latitude, Longitude, Type) values ("Hayat", "hayat.jpg", 7, 44.817952, 20.472783, 0);
